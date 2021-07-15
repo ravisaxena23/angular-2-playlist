6639d8b2ebd602baa729aab70ce8002c00b57715
ravi nandan saxena <saxenaravi23599@gmail.com>
2021-07-15 22:47:50 +0530

    JSON file from server output in angular

--- ninja-directory/src/app/directory/directory.component.ts
+++ ninja-directory/src/app/directory/directory.component.ts
@@ -2,33 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { FilterPipe } from 'app/filter.pipe';
 // import { Pipe } from '@angular/core';
 import { PipeDecorator } from '@angular/core/src/metadata/directives';
+import { LoggingService } from 'app/logging.service';
+import { DataService } from 'app/data.service';
 
 @Component({
   selector: 'app-directory',
   templateUrl: './directory.component.html',
   styleUrls: ['./directory.component.css'],
-  providers: [FilterPipe],
+  providers: [FilterPipe, LoggingService,DataService],
 })
 export class DirectoryComponent implements OnInit {
 
-  constructor() { }
-
+  constructor( private logger : LoggingService, private dataService: DataService ) { }
   term = null;
   ninjas = [
-    {
-      name: 'yoshi',
-      belt: 'black'
-    },
-    {
-      name: 'ryu',
-      belt : 'red'
-    },
-    {
-      name : 'crystal',
-      belt : 'purple'
-    }
   ];
   ngOnInit() {
+    this.dataService.fetchData();
   }
 
 }
